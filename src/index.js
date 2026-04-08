baee0842ebeedf98ba0222f3b20dbf515f8050ee
Sundaram-65 <sundaramgupta010203@gmail.com>
2026-04-08 23:51:47 +0530

    Added jwt create and verify logic

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const {PORT}=require('./config/serverConfig');
 const apiRoutes=require('./routes/index');
 const bodyParser=require('body-parser')
 
+// const {UserService}=require('./services/user-service');
+
 const app =express();
 
 const prepareAndStartServer=async()=>{
@@ -13,12 +15,28 @@ const prepareAndStartServer=async()=>{
     app.use('/api',apiRoutes);
 
     app.listen(PORT,()=>{
-        console.log(`Server started on Port: ${PORT}`)
-    })
+        console.log(`Server started on Port: ${PORT}`);
+
     // const {UserRepository}=require('./repository/user-repository');
     // const repo=new UserRepository();
     // const response=await repo.getUser(2);
     // console.log(response);
+
+
+    // const service=new UserService();
+    // creation of token
+
+    // const newtoken=service.createToken({email:'sg24365@gmail.com',id:'1'});
+    // console.log('newtoken is:',newtoken);
+
+
+    // verifytoken
+    // const token='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNnMjQzNjVAZ21haWwuY29tIiwiaWQiOiIxIiwiaWF0IjoxNzc1NjcxOTczLCJleHAiOjE3NzU2NzU1NzN9.d3mN6eL1FQcKVqyQTHxAzICa_pKI34c915zztlLiWcE';
+    // const response=service.verifyToken(token);
+    // console.log(response);
+
+    })
+
 }
 
 prepareAndStartServer();
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,6 +1,7 @@
 const {UserRepository} =require('../repository/user-repository.js');
-
+const jwt = require('jsonwebtoken');
 const userRepository= new UserRepository();
+const {JWT_KEY}=require('../config/serverConfig.js');
 class UserService{
 
     async createUser(data){
@@ -34,6 +35,25 @@ class UserService{
             console.log('Something went wrong in service layer');
             throw error;
         }
+        }
+
+        createToken(user){
+            try {
+                const result=jwt.sign(user,JWT_KEY,{expiresIn:'1d'});
+                return result;
+            } catch (error) {
+                console.log('Something went wrong in token creation');
+                throw error
+            }
+        }
+        verifyToken(token){
+            try {
+                const response=jwt.verify(token,JWT_KEY);
+                return response;
+            } catch (error) {
+                console.log('Something went wrong in token validation');
+                throw error
+            }
         }
     
 }
